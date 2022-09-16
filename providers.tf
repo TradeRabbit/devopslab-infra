b53e75192ef152cbf6f5f4e8b39c926677aded2a
TradeRabbit <68387028+TradeRabbit@users.noreply.github.com>
2022-09-16 14:22:06 -0300

    Remove credentials

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/lucianoxr80/gcpkey.json")
-
   project = "lab-devops-luciano-oliveira"
   region  = "us-west1"
   zone    = "us-west1-b"
